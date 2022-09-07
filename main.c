809181f570af271e07a7329c7ec77a05a9dc5398
zakariabelkoubi <opshanks422@gmail.com>
2022-09-07 11:46:46 +0100

    project

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    //EXEMPLE TO SHOW TEXT OR NUMBER ON SCREEN
+    printf("HI EVERY ONE\n ");
+    //EXEMPELE TO MAKE NEW LINE
+    printf("to add a nee line\n");
+    printf("and that's it\n ");
+    //EXEMPEL TO MAKE SPACE
+    printf("to make space use \t and that's it\n ");
+    //EXEMPELE TO ADD INTEGER NIMBER
+    int x=6;
+    printf("%d", x);
+    //EXEMPELE TO ADD A FLOAT NUBER
+    float y=6.5;
+    printf("%.1f \n",y);
+    //EXEMPELE TO USE  SCANF
+    int p;
+    printf("writ a integer number");
+    scanf("%d");
+    return 0;
+}
